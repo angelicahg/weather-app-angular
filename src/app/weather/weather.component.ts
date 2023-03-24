b4c228bb05a812b1606ee41753b9294a5778377e
angelica <herrerangelik@gmail.com>
2023-03-23 23:12:48 -0500

    create app weather in angular

---
+++ src/app/weather/weather.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import {WeatherService} from './../weather.service'
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.css']
+})
+export class WeatherComponent implements OnInit {
+  myWeather:any;
+  temperature:number = 0;
+  feelsLikeTemp:number =0;
+  humidity:number=0;
+  pressure:number=0;
+  summary:string ='';
+  iconURL: string ='';
+  city:string = 'Minneapolis';
+  // units:string ='imperial';
+  units:string ='metric'
+
+
+
+  constructor (private weatherService:WeatherService){}
+    ngOnInit(): void{
+     this.getWeather();
+
+
+      }
+    getWeather(){
+      this.weatherService.getweather(this.city, this.units).subscribe({
+        next: (res) => {
+          console.log(res)
+          this.myWeather= res;
+          console.log(this.myWeather)
+          this.temperature =this.myWeather.main.temp;
+          this.feelsLikeTemp=this.myWeather.main.feels_like
+          this.humidity=this.myWeather.main.feels_like
+          this.pressure=this.myWeather.main.pressure
+          this.summary=this.myWeather.weather[0].main
+          this.iconURL = 'https://openweathermap.org/img/wn/' +this.myWeather.weather[0].icon +'@2x.png'
+        },
+        error:(error) => console.log(error.message),
+        complete: () => console.info ('API CALL COMPLETED')
+      })
+
+
+    }
+
+      onRadioButtonChange(){
+        if (this.units=='imperial'){
+          this.units ='metric';
+        }else{
+          this.units ='imperial';
+        }
+        this.getWeather();
+      }
+    }
+  
+
+
